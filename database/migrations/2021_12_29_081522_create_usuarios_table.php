1e9d2c13778b97ac8b1ab6068fc3a86e370fd0a8
feliperojo <andresfelipebgomez@gmail.com>
2021-12-29 10:14:43 -0500

    menu e usuario

---
+++ database/migrations/2021_12_29_081522_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->String('usuario_emp');
+            $table->String('nombre');
+            $table->String('cargo');
+            $table->String('contrasena');
+            $table->String('create_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
