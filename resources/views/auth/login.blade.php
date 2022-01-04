c4b16660e5da27cfa607980a9873b707b692e04a
RobinsonHB <72823975+RobinsonHB@users.noreply.github.com>
2022-01-04 11:34:36 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         ...
-                        {{ csrf_token() }}
+                        @csrf
                         ...
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
